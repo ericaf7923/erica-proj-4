fc64db37c44c8812a6585fb982e05b59e6c52e67
ericaf7923 <72775447+ericaf7923@users.noreply.github.com>
2021-02-07 21:15:33 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,11 @@
+light.set_brightness(100)
+while True: 
+    print(input.rotation(Rotation.ROLL))
+    if input.rotation(Rotation.ROLL) > 20 or input.rotation(Rotation.ROLL) < -20:
+        light.set_all(light.rgb(255,0,0))
+        pause(300)
+        light.clear()
+        pause(200)
+        light.set_all(light.rgb(255,0,0))
+    else:
+        light.clear()
---
+++ main.ts
@@ -0,0 +1,14 @@
+light.setBrightness(100)
+while (true) {
+    console.log(input.rotation(Rotation.Roll))
+    if (input.rotation(Rotation.Roll) > 20 || input.rotation(Rotation.Roll) < -20) {
+        light.setAll(light.rgb(255, 0, 0))
+        pause(300)
+        light.clear()
+        pause(200)
+        light.setAll(light.rgb(255, 0, 0))
+    } else {
+        light.clear()
+    }
+    
+}
